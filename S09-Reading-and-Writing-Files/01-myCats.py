6f046a779d61ac75193e05cdfcd56816572e912a
pochiman <pochiman@yahoo.com>
2023-06-22 22:04:20 -0400

    initial commit

---
+++ S09-Reading-and-Writing-Files/01-myCats.py
@@ -0,0 +1,11 @@
+import pprint
+cats = [{'name': 'Zophie', 'desc': 'chubby'}, {'name': 'Pooka', 'desc': 'fluffy'}]
+pprint.pformat(cats)
+fileObj = open('myCats.py', 'w')
+fileObj.write('cats = ' + pprint.pformat(cats) + '\n')
+fileObj.close()
+
+# import myCats
+# myCats.cats
+# myCats.cats[0]
+# myCats.cats[0]['name']
